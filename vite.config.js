194c08be0ccdf85486e774c309536983734a99a6
InMochi <ooer379@gmail.com>
2026-03-03 11:31:49 +0700

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
+import { resolve } from 'path'
+import tailwindcss from '@theme/v4' 
 export default defineConfig({
-    base: '/Itha-Snack/', 
-    plugins: [
-        tailwindcss(),
-    ],
+
+  base: '/Itha-Snack/', 
+  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
